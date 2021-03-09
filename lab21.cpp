d0a3091d0a27794ec47985c814d5212f6dbce977
DarkTXYZ <pawaretmontfort@gmail.com>
2021-03-09 15:45:19 +0700

    Update

--- lab21.cpp
+++ lab21.cpp
@@ -2,6 +2,8 @@
 #include<cmath>
 using namespace std;
 
+#define PI 3.14159265
+
 class ComplexNumber{				
 	public:
 		double real;
@@ -29,6 +31,61 @@ ComplexNumber ComplexNumber::operator-(const ComplexNumber &c){
 }
 
 //Write your code here
+ComplexNumber ComplexNumber::operator*(const ComplexNumber &c){
+	return ComplexNumber((real*c.real) - (imag*c.imag),(imag * c.real) + (real * c.imag));
+}
+
+ComplexNumber ComplexNumber::operator/(const ComplexNumber &c){
+	double x = (c.real*c.real + c.imag*c.imag);
+	return ComplexNumber(real , imag ) * ComplexNumber(c.real , -c.imag) * ComplexNumber(1.0/x);
+}
+
+bool ComplexNumber::operator==(const ComplexNumber &c){
+	return real == c.real && imag == c.imag;
+}
+
+double ComplexNumber::abs(){
+	return sqrt(real*real + imag*imag) ; 
+}
+
+double ComplexNumber::angle(){
+	return atan2(imag,real) * 180 / PI;
+}
+
+ostream & operator<<(ostream &os , const ComplexNumber &c){
+	if(c.real != 0 && c.imag > 0)
+		return os << c.real << "+" << c.imag << "i";
+	if(c.real != 0 && c.imag == 0)
+		return os << c.real; 
+	if(c.imag < 0 && c.real != 0)
+		return os << c.real << c.imag << "i";
+	if(c.real == 0 && c.imag != 0)
+		return os << c.imag << "i";
+	if(c.real == 0 && c.imag == 0)
+		return os << 0;
+	return os << 0;
+}
+
+ComplexNumber operator+(double s ,const ComplexNumber &c){
+	return ComplexNumber(s + c.real , c.imag);
+}
+
+ComplexNumber operator-(double s ,const ComplexNumber &c){
+	return ComplexNumber(s - c.real , - c.imag);
+}
+
+ComplexNumber operator*(double s ,const ComplexNumber &c){
+	return ComplexNumber(s * c.real , s * c.imag);
+}
+
+ComplexNumber operator/(double s ,const ComplexNumber &c){
+	double x = c.real * c.real + c.imag * c.imag;
+	return ComplexNumber(c.real / x , - c.imag / x);
+}
+
+ComplexNumber operator == (double s ,const ComplexNumber &c){
+	return s == c.real && c.imag == 0 ;
+}
 
 int main(){
 	ComplexNumber a(1.5,2),b(3.2,-2.5),c(-1,1.2);	
